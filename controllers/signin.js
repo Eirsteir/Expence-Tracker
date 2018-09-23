449c917f87e877ccccb1fe1dc7b882debf56c6ce
Steinbra99 <eiriks1999@hotmail.com>
2018-09-23 14:27:13 +0200

    Change signin to look for user in login collection

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,21 +1,26 @@
 // Login model
 const Login = require('../models/Login');
+// Users model
+const User = require('../models/User');
 
 const handleSignin = (req, res, bcrypt) => {
-  const { email, name, password } = req.body;
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json('incorrect form submission')
+  }
 
-  Login.find({ email: email, hash: hash})
-    .then(user => {
-      console.log(user[0]);
-      if (user[0] && user[0].id) {
-        res.json(user[0]);
+  Login.find({ email: email })
+    .then(data => {
+      const isValid = bcrypt.compareSync(password, data[0].hash); // returns true if correct credentials
+      if (isValid) {
+        return User.find({ email: email })
+          .then(user => res.json(user[0]))
+          .catch(err => res.status(400).json('Unable to get user'))
       } else {
-        res.status(400).json('Error signin in');
+        return res.status(400).json('wrong email or password')
       }
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  })
+  .catch(console.log)
 
 }
 
