8bc103345390ef0e7646b6ad87f89ec99d64c0e5
Steinbra99 <eiriks1999@hotmail.com>
2018-09-23 15:12:31 +0200

    Add email check in register

--- controllers/register.js
+++ controllers/register.js
@@ -3,6 +3,57 @@ const User = require('../models/User');
 // Login model
 const Login = require('../models/Login');
 
+const isEmailInUse = (email) => {
+  User.find({ email: email })
+  .then(user => {
+    if (user || user[0] || user[0].email === email) {
+      return true
+    } else {
+      console.log(user);
+      return false
+    }
+  })
+  .catch(console.log)
+}
+
+const registerUserInDB = (req, res, db, name, email, hash) => {
+
+  const newUser = new User({
+    name: name,
+    email: email,
+  });
+
+  const newLogin = new Login({
+    email: email,
+    hash: hash
+  });
+
+  let session = null;
+
+  return db.startSession()
+  .then(_session => {
+    session = _session;
+    // start  a transaction
+    session.startTransaction()
+    newLogin.save()
+  })
+  .then(() => {
+    return newUser.save()
+      .then(user => res.json(user))
+  })
+  .then(() => {
+    session.commitTransaction();
+    session.endSession();
+    return true
+  })
+  .catch(err => {
+    console.log(err);
+    session.abortTransaction();
+    session.endSession();
+    return res.status(400).json('Unable to register')
+  });
+}
+
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
 
@@ -12,40 +63,9 @@ const handleRegister = (req, res, db, bcrypt) => {
 
   var hash = bcrypt.hashSync(password);
 
-  const newUser = new User({
-    name: name,
-    email: email,
-  });
-
-
-  const newLogin = new Login({
-    email: email,
-    hash: hash
-  });
-
-  let session = null;
-  return db.startSession()
-  .then(_session => {
-    session = _session;
-    // start  a transaction
-    session.startTransaction()
-    return newUser.save()
-      .then(user => res.json(user))
-  })
-  .then(() => {
-    return newLogin.save()
-  })
-    .then(() => {
-      session.commitTransaction();
-      session.endSession();
-      return true
-    })
-    .catch(err => {
-      console.log(err);
-      session.abortTransaction();
-      session.endSession();
-      return res.status(400).json('Unable to register')
-    })
+  return isEmailInUse(email) ?
+    res.status(400).json('email already in use')
+    : registerUserInDB(req, res, db, name, email, hash);
 
 }
 
--- models/User.js
+++ models/User.js
@@ -11,9 +11,60 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
-  date: {
+  joined: {
     type: Date,
     default: Date.now
+  },
+  age: {
+    type: Number,
+    min: 0,
+    max: 150
+  },
+  expences: {
+    type: Array,
+    default: [
+      {
+        timespan: 'All Time',
+        tags: {
+          Food: 0,
+          Household: 0,
+        }
+      },
+      {
+        timespan: "This Month",
+        tags: {
+          Food: 0,
+          Household: 0,
+        }
+      },
+      {
+        timespan: "This Week",
+        tags: {
+          Food: 0,
+          Household: 0,
+        }
+      },
+      {
+        timespan: "Today",
+        tags: {
+          Food: 0,
+          Household: 0,
+        }
+      }
+    ]
+  },
+  totalExpences: {
+    type: Map,
+    of: String,
+    default: {
+      allTime: '0',
+      thisMonth: '0',
+      thisWeek: '0',
+      today: '0'
+    }
+  },
+  customUserTags: {
+    type: Array
   }
 });
 
