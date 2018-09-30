1a31902c5608ccad7afb720a737c0ffcd0aee7e9
Steinbra99 <eiriks1999@hotmail.com>
2018-09-30 22:35:09 +0200

    Dunno bro

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const db = mongoose.connection;
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static('Expence-Tracker-Client/build'));
+app.use(express.static('/Expence-Tracker-Client/build'));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile('/Expence-Tracker-Client/build/index.html');
