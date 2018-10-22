9d438024297402988a32bece923100bfb5e891f2
Steinbra99 <eiriks1999@hotmail.com>
2018-10-22 20:34:33 +0200

    Optimized image

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -7,8 +7,6 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
-import "./Hero.css";
-
 const Hero = ({ onRouteChange }) => {
   return (
     <Grid
